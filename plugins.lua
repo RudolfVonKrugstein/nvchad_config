e4f8ab6903daefcedee0a12977fd5e4a87378ccd
Nathan Hüsken <nathan@wintercloud.de>
2023-04-17 13:30:48 +0200

    more plugins

--- plugins.lua
+++ plugins.lua
@@ -10,5 +10,35 @@ local plugins = {
         silent_chdir = false
       }
     end
+  },
+  {
+    "kylechui/nvim-surround",
+    lazy = false,
+    config = function()
+      require("nvim-surround").setup {
+      }
+    end
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    lazy = false,
+    config = function()
+      vim.fn["mkdp#util#install"]()
+    end
+  },
+  {
+    "pest-parser/pest.vim",
+    lazy = false
+  },
+  {
+    "pocco81/auto-save.nvim",
+    lazy = false
+  },
+  {
+    "Lilja/zellij.nvim",
+    lazy =false,
+    config = function()
+      require('zellij').setup({})
+    end
   }
 }
